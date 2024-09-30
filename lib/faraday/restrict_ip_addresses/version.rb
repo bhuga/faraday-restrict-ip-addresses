9aa02a3812ebb8ea6fa2bc82b2ab4a7b33cdc574
Ryan Davis <ryand-ruby@zenspider.com>
2024-09-30 12:16:20 -0700

    Remove faraday require in version.rb and define Middleware
    slight cheat because of the superclass... maybe it isn't needed tho?

--- lib/faraday/restrict_ip_addresses/version.rb
+++ lib/faraday/restrict_ip_addresses/version.rb
@@ -1,5 +1,5 @@
-require 'faraday'
 module Faraday
+  class Middleware; end # so we don't have to load faraday gem to get version
   class RestrictIPAddresses < Faraday::Middleware
     VERSION = '0.2.0'
   end
