a163495fb5bed384260f335920dc0e7399b1af84
Ryan Davis <ryand-ruby@zenspider.com>
2024-09-30 12:22:55 -0700

    Bump to 0.3.0

--- lib/faraday/restrict_ip_addresses/version.rb
+++ lib/faraday/restrict_ip_addresses/version.rb
@@ -1,6 +1,6 @@
 module Faraday
   class Middleware; end # so we don't have to load faraday gem to get version
   class RestrictIPAddresses < Faraday::Middleware
-    VERSION = '0.2.0'
+    VERSION = '0.3.0'
   end
 end
